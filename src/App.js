e8d7410ccac171ba10193f53652383209175aea4
OSHIN <izkp44@gmail.com>
2023-04-22 22:44:48 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useState } from 'react'
+import SatitLifeCycle from './SatitLifeCycle'
+
 function App() {
-  return <div className="App"></div>
+  const [displayed, setDisplayed] = useState(true)
+  const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(0)
+
+  return (
+    <div className="">
+      <div style={{ display: 'flex', gap: 60 }}>
+        <button
+          style={{ fontSize: 60 }}
+          onClick={() => setCount((prev) => prev + 1)}
+        >
+          Click count: {count}
+        </button>
+      </div>
+      <button
+        style={{ fontSize: 60 }}
+        onClick={() => setCount2((prev) => prev + 1)}
+      >
+        Click count2: {count2}
+      </button>
+      <hr />
+      <button style={{ fontSize: 72 }} onClick={() => setDisplayed(!displayed)}>
+        {displayed ? 'Hide' : 'Show'}
+      </button>
+      {displayed && <SatitLifeCycle count={count} count2={count2} />}
+    </div>
+  )
 }
 
 export default App
